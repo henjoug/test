2ab33c6b6f37a14b81d17d2dd907a4006d17d20b
henjoug <henjoug@gmail.com>
2026-09-21 14:45:24 +0200

    connectors: add Microsoft V0.1 skeletons

---
+++ src/connectors/microsoft/index.ts
@@ -0,0 +1,1 @@
+export class OutlookConnector{readonly implemented=false} export class MicrosoftCalendarConnector{readonly implemented=false} export class OneDriveConnector{readonly implemented=false} export class SharePointConnector{readonly implemented=false}
